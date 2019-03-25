96cb8b963b45e6fe05bf341108ebc3cf7b480e41
Seru <ghks1353@gmail.com>
2019-03-26 02:25:41 +0900

    init structure files

---
+++ gatsby-config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Seru.dev",
+  },
+  plugins: [
+    "gatsby-plugin-typescript",
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
+};
---
+++ src/Pages/index.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Index extends Component {
+  render(): JSX.Element {
+    return <div>asdf</div>;
+  }
+}
